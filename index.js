092c49d5ec1cade61ea4e745c9f1bb650cef53ae
Dan Bucholtz <dan.bucholtz@gmail.com>
2015-01-03 02:41:24 -0600

    testing...

---
+++ index.js
@@ -0,0 +1,97 @@
+var Q = require("q");
+
+var utils = require("soa-example-core-utils");
+
+var config = require("soa-example-service-config").config();
+
+var redisUtil = require('soa-example-redis-util');
+
+var whiteList = [];
+
+var initializeWhiteList = function(){
+	whiteList.push(config.authenticationServiceIp);
+	whiteList.push(config.authorizationServiceIp);
+	whiteList.push(config.bannedIpServiceIp);
+	whiteList.push(config.loggingServiceIp);
+	whiteList.push(config.userServiceIp);
+}();
+
+var isIpAddressWhiteListed = function(ipAddress){
+	return (whiteList.indexOf(ipAddress) > -1);
+};
+
+var isIpAddressBanned = function(ipAddress){
+	var deferred = Q.defer();
+
+	if ( isIpAddressWhiteListed(ipAddress) ){
+		deferred.resolve(false);
+	}
+	else{
+		redisUtil.get(ipAddress).then(function(object){
+
+			if ( object ){
+				deferred.resolve(true);
+			}
+			else{
+				deferred.resolve(false);
+			}
+		});
+	}
+	
+	return deferred.promise;
+};
+
+var invalidBasicCredentials = function(ipAddress, username, password){
+	var deferred = Q.defer();
+
+	var url = utils.createBaseUrl(config.bannedIpServiceIp, config.bannedIpServicePort) + "/invalid";
+
+	var toPost = {
+		type : getBasicType(),
+		ipAddress: ipAddress,
+		username: username,
+		password: password
+	};
+
+	utils.postJsonWithAccessToken(config.systemAccessToken, toPost, url).then(function(response){
+		deferred.resolve(resolve);
+	});
+
+	return deferred.promise;
+};
+
+var invalidBearerCredentials = function(ipAddress, accessToken){
+	var deferred = Q.defer();
+
+	var url = utils.createBaseUrl(config.bannedIpServiceIp, config.bannedIpServicePort) + "/invalid";
+
+	var toPost = {
+		type : getBearerType(),
+		ipAddress: ipAddress,
+		accessToken: accessToken
+	};
+
+	utils.postJsonWithAccessToken(config.systemAccessToken, toPost, url).then(function(response){
+		deferred.resolve(resolve);
+	});
+
+	return deferred.promise;
+};
+
+var getBasicType = function(){
+	return "Basic";
+};
+
+var getBearerType = function(){
+	return "Bearer";
+};
+
+
+module.exports = {
+	isIpAddressBanned : isIpAddressBanned,
+	isIpAddressWhiteListed: isIpAddressWhiteListed,
+	invalidBasicCredentials: invalidBasicCredentials,
+	invalidBearerCredentials: invalidBearerCredentials,
+	getBasicType: getBasicType,
+	getBearerType: getBearerType
+}
